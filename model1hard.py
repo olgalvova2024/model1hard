526f62f715bb602f2f95aa01af5b267e1f35836b
olgalvova2024 <olga.lvova1317@mail.ru>
2024-06-23 10:33:42 +0300

    дополнительное задание по модулю 1

---
+++ model1hard.py
@@ -0,0 +1,11 @@
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students={'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+students=list(students)
+students.sort()
+gr0=(grades[0][0]+grades[0][1]+grades[0][2]+grades[0][3]+grades[0][4])/len(grades[0])
+gr1=(grades[1][0]+grades[1][1]+grades[1][2]+grades[1][3])/len(grades[1])
+gr2=(grades[2][0]+grades[2][1]+grades[2][2]+grades[2][3])/len(grades[2])
+gr3=(grades[3][0]+grades[3][1]+grades[3][2])/len(grades[3])
+gr4=(grades[4][0]+grades[4][1]+grades[4][2]+grades[4][3]+grades[4][4])/len(grades[4])
+sp={students[0]:gr0,students[1]:gr1,students[2]:gr2,students[3]:gr3,students[4]:gr4,}
+print(sp)
